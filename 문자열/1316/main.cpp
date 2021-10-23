a77ce2945bf45649515debe6a6d67d7b1ae5aeb1
minaamim <minaaa5814@gmail.com>
2021-10-23 17:51:04 +0900

    문자열

--- 문자열/1316/main.cpp
+++ 문자열/1316/main.cpp
@@ -9,8 +9,25 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
     string str;
-    cin >> str;
-    
+    int cnt = 0;
+   
+    for(int i = 0; i < n; i++){ //단어의 갯수만큼 돌린다
+        cin >> str;
+        int flag = true;
+        
+        for(int j = 0; j < str.length(); j++){ //단어의 길이만큼 돌린다
+            for(int k = 0; k < j; k++){
+                if(str[j] != str[j-1] && str[j] == str[k]){ //바로 전 알파벳과 같지 않고 그 이전에 중복되는 알파벳이 있다면 브레이크
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if(flag) cnt++;
+    }
+    cout << cnt;
     return 0;
 }
