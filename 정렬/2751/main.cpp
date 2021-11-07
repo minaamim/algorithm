ac71c63cf66f19d1d5eae930ad99e4d7bf748203
minaamim <minaaa5814@gmail.com>
2021-11-07 19:39:56 +0900

    정렬

---
+++ 정렬/2751/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  2751
+//
+//  Created by 홍민아 on 2021/11/07.
+//
+
+#include <iostream>
+//#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int arr[n];
+    
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+  //  sort(arr[0], arr[n]);
+    
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << "\n" ;
+    
+    return 0;
+}
