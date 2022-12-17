ff2a824f098d82ea6e64c4e5050377e671eec14a
minaamim <minaaa5814@gmail.com>
2022-12-17 21:12:09 +0900

    [Lv.2] 디펜스 게임

---
+++ 프로그래머스/Defence.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+public class Defence {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int round = 0; round < enemy.length; round++) {
+            queue.add(enemy[round]);
+            if(queue.size() > k)
+                n -= queue.poll();
+            if(n < 0)
+                return round;
+        }
+        answer = enemy.length;
+        return answer;
+    }
+}
