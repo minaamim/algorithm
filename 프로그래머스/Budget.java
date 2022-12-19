01aa8784fb86234ceb0eae8f8e532b9b69fa7f51
minaamim <minaaa5814@gmail.com>
2022-12-19 17:25:14 +0900

    [Lv.1] 예산

---
+++ 프로그래머스/Budget.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Budget {
+    public int solution(int[] d, int budget) {
+        //최대 몇 개의 부서에 물품을 지원할 수 있는지 구하라
+        int answer = 0;
+
+        Arrays.sort(d);
+
+        for(int i = 0; i < d.length; i++) {
+            budget -= d[i];
+
+            if(budget < 0)
+                return i;
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
