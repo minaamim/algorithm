02fe475d47b61891611ef882a1f547e3b0eb9120
minaamim <minaaa5814@gmail.com>
2022-12-19 17:00:43 +0900

    [Lv.1] 같은 숫자는 싫어

---
+++ 프로그래머스/StackTest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class StackTest {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < arr.length; i++) {
+            if(stack.isEmpty() || stack.peek() != arr[i])
+                stack.add(arr[i]);
+        }
+        int[] answer = new int[stack.size()];
+        for(int i = 0; i < stack.size(); i++) {
+            answer[i] = stack.get(i);
+        }
+
+        return answer;
+    }
+}
