ca65ecf87cb89d8c8aa7d2d2171c637dfda14876
minaamim <minaaa5814@gmail.com>
2022-12-26 16:05:22 +0900

    [Lv.1] 크기가 작은 부분문자열

---
+++ 프로그래머스/Substring.java
@@ -0,0 +1,12 @@
+public class Substring {
+    public int solution(String t, String p) {
+        int answer = 0;
+        int len = p.length();
+
+        for(int i = 0; i <= t.length() - len; i++) {
+            if(Long.parseLong(t.substring(i, i+len)) <= Long.parseLong(p))
+                answer++;
+        }
+        return answer;
+    }
+}
