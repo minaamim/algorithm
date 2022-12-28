1be76d385fdec32853b4a190ec7175c5f9ebf4c2
minaamim <minaaa5814@gmail.com>
2022-12-29 00:26:19 +0900

    [Lv.1] 과일 장수

---
+++ 프로그래머스/FruitSeller.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class FruitSeller {
+    class Solution {
+        public int solution(int k, int m, int[] score) {
+            /**
+             * 과일 상태에 따라 1점 - k점
+             * m = 한 상자에 담을 수 있는 사과의 수
+             * score = 사과들의 점수
+             * 과일 장수가 얻을 수 있는 최대 이익을 구하라
+             */
+            int answer = 0;
+
+            Arrays.sort(score);
+
+            for(int i = score.length - 1; i >=0; i--) {
+                if((score.length - i) % m == 0)
+                    answer+=score[i] * m;
+            }
+            return answer;
+        }
+    }
+}
