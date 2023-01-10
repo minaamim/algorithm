f2cb60527d79b7ea09b692fd1a390413c2fa3bb4
minaamim <minaaa5814@gmail.com>
2023-01-10 16:12:41 +0900

    [BOJ, 15560] N과 M(2)

---
+++ 백트래킹/BOJ15650.java
@@ -0,0 +1,44 @@
+package backtracking;
+
+import java.util.Scanner;
+
+public class BOJ15650 {
+    static boolean[] visit;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    static int n, m;
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        visit = new boolean[n];
+        arr = new int[m];
+
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth, int start) {
+
+        if(depth == m) {
+            for(int temp : arr) {
+                sb.append(temp).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i < n; i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i+1;
+                dfs(depth+1, i+1);
+                visit[i] = false;
+            }
+        }
+    }
+
+}
