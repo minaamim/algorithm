3187973e05d150feae3019ec6e0ed87286a52947
minaamim <minaaa5814@gmail.com>
2023-02-09 17:53:44 +0900

    [Lv.1] 햄버거 만들기

--- 프로그래머스/level1/src/Hamburger.java
+++ 프로그래머스/level1/src/Hamburger.java
@@ -1,9 +1,29 @@
+import java.util.Stack;
+
 public class Hamburger {
     public int solution(int[] ingredient) {
         int answer = 0;
 
+        Stack<Integer> order = new Stack<>();
 
+        for(int in : ingredient) {
+            order.push(in);
 
+            if(order.size() >= 4) {
+                int size = order.size();
+                if(order.get(size - 1) == 1
+                && order.get(size - 2) == 3
+                && order.get(size - 3) == 2
+                && order.get(size - 4) == 1) {
+                    answer++;
+                    order.pop();
+                    order.pop();
+                    order.pop();
+                    order.pop();
+                }
+            }
+
+        }
         return answer;
     }
 }
