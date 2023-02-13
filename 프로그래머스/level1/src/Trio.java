6c8579aa7c99638e524e8e129c2517f5e9b0ce6d
minaamim <minaaa5814@gmail.com>
2023-02-13 15:14:56 +0900

    [Lv.1] 삼총사

---
+++ 프로그래머스/level1/src/Trio.java
@@ -0,0 +1,24 @@
+public class Trio {
+
+    public int solution(int[] number) {
+        /**
+         * number = 학생들의 번호를 나타내는 정수 배열
+         * answer = 삼총사를 만들 수 있는 방법의 수
+         */
+
+        int answer = 0;
+
+        for(int first = 0; first < number.length - 2; first++) {
+            for(int second = first + 1; second < number.length - 1; second++) {
+                for(int third = second + 1; third < number.length; third++) {
+                    if(number[first] + number[second] + number[third] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+}
