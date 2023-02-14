1a68ec72d1ba0302bc98043ff50d4df05a68b56d
minaamim <minaaa5814@gmail.com>
2023-02-14 14:50:05 +0900

    [Lv.1] 옹알이 (2)

---
+++ 프로그래머스/level1/src/Babillage2.java
@@ -0,0 +1,34 @@
+public class Babillage2 {
+
+    public static void main(String args[]) {
+    }
+
+    public int solution(String[] babbling) {
+
+        String[] babList = {"aya", "ye", "woo", "ma"};
+        String[] repeatedList = {"ayaaya", "yeye", "woowoo", "mama"};
+
+        int answer = 0;
+
+        for(String bab : babbling) {
+
+            for(String rStr : repeatedList) {
+                bab = bab.replace(rStr, "X");
+                System.out.println("replacedBab: " + bab);
+            }
+
+            for(String str : babList) {
+                bab = bab.replace(str, " ");
+                System.out.println("result: " + bab);
+            }
+
+            bab = bab.replace(" ", "");
+
+            if(bab == "")
+                answer++;
+        }
+
+        return answer;
+    }
+
+}
