134c074253ea6c21ac018d49302ceee2ecc5ea03
minaamim <minaaa5814@gmail.com>
2023-03-25 17:14:01 +0900

    [Lv.2] 프린터

--- 프로그래머스/level2/src/stack/Printer.java
+++ 프로그래머스/level2/src/stack/Printer.java
@@ -5,7 +5,18 @@ import java.util.PriorityQueue;
 
 public class Printer {
 
-    public int solution(int[] priorities, int location) {
+    public static void main(String args[]) {
+//        int[] priorities = {2, 1, 3, 2};
+//        int location = 2;
+
+        int[] priorities = {1, 1, 9, 1, 1, 1,};
+        int location = 0;
+
+        System.out.println(solution(priorities, location));
+    }
+
+
+    public static int solution(int[] priorities, int location) {
         int answer = 0;
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
@@ -16,14 +27,20 @@ public class Printer {
 
         while(!pq.isEmpty()) {
             for(int i = 0; i < priorities.length; i++) {
+
+                System.out.println("i: " + i);
+                System.out.println("priorities[i]: " + priorities[i]);
+                System.out.println("pq.peek(): " + pq.peek());
+
                 if (priorities[i] == pq.peek()) {
                     if (i == location) {
-                        answer++;
-                        return answer;
+                        return ++answer;
                     }
+
                     pq.poll();
                     answer++;
                 }
+
             }
         }
 
