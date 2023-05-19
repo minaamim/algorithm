11fff0e096edb351d31536911ee3987f306b67f0
minaamim <minaaa5814@gmail.com>
2023-05-19 21:23:08 +0900

    [Lv.2] 소수 찾기

--- 프로그래머스/level2/src/bruteforce/PrimeNumber.java
+++ 프로그래머스/level2/src/bruteforce/PrimeNumber.java
@@ -4,67 +4,53 @@ import java.util.HashSet;
 
 public class PrimeNumber {
 
-    static HashSet<Integer> set = new HashSet<>();
     static char[] ch;
     static boolean[] visited;
+    static HashSet<Integer> hash;
 
-    public int solution(String numbers) {
+    public static void main(String args[]) {
+        System.out.println(solution("17"));
+        System.out.println(solution("011"));
+    }
 
-        /**
-         * 1. 만들 수 있는 조합 찾기
-         * 2. 소수인지 판단하기
-         */
-
+    public static int solution(String numbers) {
         int answer = 0;
         ch = new char[numbers.length()];
         visited = new boolean[numbers.length()];
+        hash = new HashSet<>();
 
-        for(int i = 0; i < numbers.length(); i++)
-            ch[i] = numbers.charAt(i);
+        for (int i = 0; i < numbers.length(); i++) ch[i] = numbers.charAt(i);
 
-        answer = set.size();
         dfs("", 0);
-
+        answer = hash.size();
         return answer;
     }
 
-    void dfs(String str, int index) {
+    static void dfs(String str, int index) {
         int num;
-        if(str!="") {
-            num = Integer.parseInt(str);
 
-            if(isPrime(num))
-                set.add(num);
+        if(str != "") {
+            num = Integer.parseInt(str);
+            if(isPrime(num)) hash.add(num);
         }
 
-        if(index == ch.length)
-            return;
+        if(index == ch.length) return;
 
         for(int i = 0; i < ch.length; i++) {
-            if(visited[i])
-                continue;
+            if(visited[i]) continue;
 
             visited[i] = true;
             dfs(str + ch[i], index + 1);
             visited[i] = false;
         }
-
-
     }
 
-    boolean isPrime(int num) {
+    static boolean isPrime(int num) {
+        if(num == 0 || num == 1) return false;
 
-        if(num == 0 || num == 1)
-            return false;
-
-
-        for(int i = 2; i <= Math.sqrt(num); i++) {
-
-            if (num % i == 0)
-                return false;
-
-        }
+        for(int i = 2; i < Math.sqrt(num); i++) if(num % i == 0) return false;
 
         return true;
     }
+
 }
