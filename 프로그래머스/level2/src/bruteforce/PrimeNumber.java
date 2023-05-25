8e7ca976deb61938d7761528d3606af7955dad4e
minaamim <minaaa5814@gmail.com>
2023-05-25 20:27:36 +0900

    [Lv.2] 소수 찾기

--- 프로그래머스/level2/src/bruteforce/PrimeNumber.java
+++ 프로그래머스/level2/src/bruteforce/PrimeNumber.java
@@ -48,7 +48,7 @@ public class PrimeNumber {
     static boolean isPrime(int num) {
         if(num == 0 || num == 1) return false;
 
-        for(int i = 2; i < Math.sqrt(num); i++) if(num % i == 0) return false;
+        for(int i = 2; i <= Math.sqrt(num); i++) if(num % i == 0) return false;
 
         return true;
     }
