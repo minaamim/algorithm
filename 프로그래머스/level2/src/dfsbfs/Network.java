e227688b0884b1fa528a2e2c1e4dbee73bcd1724
minaamim <minaaa5814@gmail.com>
2023-05-26 20:34:20 +0900

    [Lv.2] 네트워크

--- 프로그래머스/level2/src/dfsbfs/Network.java
+++ 프로그래머스/level2/src/dfsbfs/Network.java
@@ -1,31 +1,39 @@
 package dfsbfs;
 
 public class Network {
-    boolean[] check;
 
-    public int solution(int n, int[][] computers) {
+    public static void main(String args[]) {
+        int[][] computer1 = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        int[][] computer2 = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+
+        System.out.println(solution(3, computer1));
+        System.out.println(solution(3, computer2));
+    }
+
+    static boolean[] visited;
+
+    public static int solution(int n, int[][] computers) {
         int answer = 0;
-        check = new boolean[n];
 
-        for(int i = 0; i < n; i++) {
-            if (!check[i]) {
-                dfs(computers, i);
+        visited = new boolean[n];
+
+        for(int i = 0; i < computers.length; i++) {
+            if(!visited[i]) {
                 answer++;
+                dfs(i, computers);
             }
         }
         return answer;
     }
 
-    boolean[] dfs(int[][] computers, int i) {
-        check[i] = true;
+    static void dfs(int depth, int[][] computers) {
+
+        visited[depth] = true;
 
         for(int j = 0; j < computers.length; j++) {
-
-            //이어져있으면 모두 check
-            if(check[j] == false && computers[i][j] == 1 && i!=j) {
-                check = dfs(computers, j);
-            }
+            if (depth != j && !visited[j] && computers[depth][j] == 1)
+                dfs(j, computers);
         }
-        return check;
+        return;
     }
 }
