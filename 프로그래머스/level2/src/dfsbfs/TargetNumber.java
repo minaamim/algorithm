e28913696ec92cfbeca9f09bcbfa2230b50bd977
minaamim <minaaa5814@gmail.com>
2023-05-26 20:34:12 +0900

    [Lv.2] 타겟 넘버

--- 프로그래머스/level2/src/dfsbfs/TargetNumber.java
+++ 프로그래머스/level2/src/dfsbfs/TargetNumber.java
@@ -1,24 +1,34 @@
 package dfsbfs;
 
 public class TargetNumber {
-    int answer = 0;
 
-    public int solution(int[] numbers, int target) {
+    public static void main(String args[]) {
+        int[] numbers1 = {1, 1, 1, 1, 1};
+        int[] numbers2 = {4, 1, 2, 1};
 
-        dfs(numbers, 0, target, 0);
+        System.out.println(solution(numbers1, 3));
+        System.out.println(solution(numbers2, 4));
+    }
+    static int answer = 0;
+
+    public static int solution(int[] numbers, int target) {
+
+        dfs(numbers, target, 0, 0);
 
         return answer;
     }
 
-    public void dfs(int[] numbers, int depth, int target, int sum) {
-        if (depth == numbers.length) {
-            if (target == sum)
-                answer++;
-        } else {
-            dfs(numbers, depth+1, target, sum + numbers[depth]);
-            dfs(numbers, depth+1, target, sum - numbers[depth]);
+    static void dfs(int[] numbers, int target, int depth, int sum) {
 
+        System.out.println("target: " + target + " depth: " + depth + " sum: " + sum );
+        if(depth == numbers.length) {
+            if(sum == target) answer++;
+
+            return;
         }
+
+        dfs(numbers, target, depth + 1, sum + numbers[depth]);
+        dfs(numbers, target, depth + 1, sum - numbers[depth]);
     }
 
 }
