ce12219ce5fa10595606f0b41da46ba7427e52e6
minaamim <minaaa5814@gmail.com>
2023-07-11 20:44:11 +0900

    [Lv.2] 124 나라의 숫자

---
+++ 프로그래머스/level2/src/P12899.java
@@ -0,0 +1,21 @@
+public class P12899 {
+
+    public static void main(String args[]) {
+        System.out.println(solution(4));
+    }
+
+    public static String solution(int n) {
+
+        String[] numbers = {"4", "1", "2"};
+
+        String answer = "";
+
+        while(n > 0) {
+           answer = numbers[n % 3] + answer;
+           n = (n - 1) / 3;
+        }
+
+        return answer;
+    }
+
+}
