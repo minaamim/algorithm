bf8c8e260646c578d160fc56ab1220001ac3ee2a
minaamim <minaaa5814@gmail.com>
2023-11-24 19:12:25 +0900

    [Lv.1] 숫자 문자열과 영단어

---
+++ 프로그래머스/level1/src/kakao/P81301.java
@@ -0,0 +1,17 @@
+package kakao;
+
+public class P81301 {
+    public int solution(String s) {
+        int answer;
+        String[] number = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for(int i = 0; i < number.length; i++) {
+            if(s.contains(number[i])) {
+                s = s.replace(number[i], String.valueOf(i));
+            }
+        }
+
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
